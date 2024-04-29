c81c7c6f5dd2d488d4ee15e570e051cfca2ac847
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-04-29 23:49:24 +0300

    correct notification import added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Notification/>
+        <Notification />
         <Navbar />
         {children}
         <Footer />
