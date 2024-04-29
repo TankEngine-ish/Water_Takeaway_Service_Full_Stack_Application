86ce5d9376acd22751bcf881b303fb5c2e392eb0
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-04-29 23:49:05 +0300

    fix notification component

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 const Notification = () => {
-  return <div>Notification</div>
+  return (
+    <div className='h-12 bg-red-500 text-white'>FREE DELIVERY FOR ALL PATIENTS ON HEMODIALYSIS. ORDER YOUR WATER NOW! </div>
+  )
 }
-export default Notification
+export default Notification;
