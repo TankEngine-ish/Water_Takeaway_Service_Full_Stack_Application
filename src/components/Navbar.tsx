339312fff58a310bfcb5415156afb7f5cc9406d9
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-04-30 03:00:37 +0300

    add text size to logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
   <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
     {}
-    <div>
+    <div className="text-xl">
       <Link href={''}>Velingrad</Link>
     </div>
     {}
