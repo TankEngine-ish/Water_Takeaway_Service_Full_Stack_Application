000a9ff921c22d49b0fe87b8f0b5cf8d3d9c1a9c
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-04-30 03:00:50 +0300

    create menu state on click

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 
 const Menu = () => {
+    const [open,setOpen] = useState(false)
   return (
-  <div><Image src="/open.png" alt ="" width={20} height={20}/></div>
-  )
-}
-export default Menu
+    <div>
+    {!open ? (
+        <Image src="/open.png" alt ="" width={20} height={20} onClick={()=>setOpen(true)}/>
+    ) : (
+    <Image src="/close.png" alt ="" width={20} height={20}onClick={()=>setOpen(false)}/>
+  )}
+  </div>
+  );
+};
+export default Menu;
