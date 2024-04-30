7aa03d1bd5621e384a939339f7e170cd1dd64b82
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-04-30 23:53:39 +0300

    adjust notification colors

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const Notification = () => {
   return (
-    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>FREE DELIVERY FOR ALL PATIENTS ON HEMODIALYSIS. ORDER YOUR WATER NOW! </div>
+    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>FREE DELIVERY FOR ALL PATIENTS ON HEMODIALYSIS. ORDER YOUR WATER NOW!</div>
   )
 }
 export default Notification;
