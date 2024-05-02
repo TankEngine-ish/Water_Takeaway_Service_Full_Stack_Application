4c819b013278e617cc93617ea1bf16ed802f8d2a
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-05-02 18:07:06 +0300

    add png icons in footer section for socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
-const Footer = () => {
-  return <div>Footer</div>
-}
-export default Footer
+import Link from "next/link";
+import React from "react";
 
+const Footer = () => {
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-between p-4 lg:px-20 xl:px-40 text-red-500">
+      <Link href="/" className="font-bold text-xl mb-4 md:mb-0">VELINGRAD</Link>
+      <div className="flex space-x-4">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <img src="/facebook2.png" alt="Facebook" className="w-6 h-6" />
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <img src="/instagram.png" alt="Instagram" className="w-6 h-6" />
+        </a>
+      </div>
+      <p className="mt-4 md:mt-0">© {new Date().getFullYear()} ALL RIGHTS RESERVED.</p>
+    </div>
+  );
+};
+
+export default Footer;
