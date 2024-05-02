ce3f89419757df1cf6a464e8671b18a8db78a638
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-05-02 17:32:46 +0300

    fix menu bar showing non stop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,30 +2,31 @@ import React from 'react';
 import Menu from './Menu';
 import Link from 'next/link';
 import CartIcon from './CartIcon';
+
 const Navbar = () => {
 
   const user = false;
   return (
-  <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
+  <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
     {/* Left Links */}
-    <div className="hidden md:flex gap-4">
+    <div className="hidden md:flex gap-4 flex-1">
     <Link href="/">Homepage</Link>
     <Link href="/menu">Menu</Link>
     <Link href="/">Contact</Link>
     </div>
     {/* Logo */}
-    <div className="text-xl">
+    <div className="text-xl md:font-bold flex-1 md:text-center">
       <Link href="/">Velingrad</Link>
     </div>
     {/* Mobile Menu */}
-    <div className="md:hidden"></div>
-    <div>
+    <div className="md:hidden">
       <Menu/>
     </div>
     {/* Right Links */}
-    <div className="hidden md:flex gap-4">
-      <div>
-
+    <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+      <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+        <img src="/phone.png" alt="" width={20} height={20}/>
+        <span>0700 123 456</span>
       </div>
       {!user ? (
     <Link href="/login">Login</Link>
