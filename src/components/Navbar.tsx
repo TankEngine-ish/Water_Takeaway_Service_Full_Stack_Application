2514bc3769b0747b5ee93e33ae3feef66bb68eaa
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-05-02 17:14:40 +0300

    add links appearing on :md size screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
 import Menu from './Menu';
 import Link from 'next/link';
+import CartIcon from './CartIcon';
 const Navbar = () => {
+
+  const user = false;
   return (
   <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
-    {}
-    <div className="text-xl">
-      <Link href={''}>Velingrad</Link>
+    {/* Left Links */}
+    <div className="hidden md:flex gap-4">
+    <Link href="/">Homepage</Link>
+    <Link href="/menu">Menu</Link>
+    <Link href="/">Contact</Link>
     </div>
-    {}
+    {/* Logo */}
+    <div className="text-xl">
+      <Link href="/">Velingrad</Link>
+    </div>
+    {/* Mobile Menu */}
+    <div className="md:hidden"></div>
     <div>
       <Menu/>
+    </div>
+    {/* Right Links */}
+    <div className="hidden md:flex gap-4">
+      <div>
+
+      </div>
+      {!user ? (
+    <Link href="/login">Login</Link>
+      ) : (
+    <Link href="/orders">Orders</Link>
+      )}
+    <CartIcon/>
     </div>
 </div>
 )
