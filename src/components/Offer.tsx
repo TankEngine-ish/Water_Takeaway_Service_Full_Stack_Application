38d3b20423d2c87c3f5e16b41d3923c3775939c1
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-05-27 23:36:54 +0300

    offer file change

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -7,7 +7,7 @@ const Offer = () => {
     <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.jpg')] md:h-[70vh]">
       {/* TEXT CONTAINER */}
       <div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
-        <h1 className="text-white text-5xl font-bold xl:text-6xl">Laundry Day Promo!</h1>
+        <h1 className="text-white text-5xl font-bold xl:text-6xl">Laundry Smayay Promo!</h1>
         <p className="text-white xl:text-xl">
           Hand-Wash™ your clothes and get 50% off on your next order over 10 Litres.
         </p> 
