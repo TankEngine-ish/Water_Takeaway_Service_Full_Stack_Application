1fe82533fc42bce87ada276cb342382363080426
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-05-30 23:48:35 +0300

    fix the line in notification

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 const Notification = () => {
   return (
-    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>BEST RESOURCE for PA announcers, play-by-play broadcasters, members of the media and fans!
-    members of the media and fans!</div>
+    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>BEST RESOURCE for PA announcers, play-by-play broadcasters, members of the media and fans!</div>
   )
 }
 export default Notification;
